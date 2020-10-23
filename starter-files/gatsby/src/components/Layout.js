9adc00827932f70ebc0f2023e743dcf20ac7b66a
Amorem <dev@amorem.com>
2020-10-23 23:20:14 +0200

    Toppings

--- starter-files/gatsby/src/components/Layout.js
+++ starter-files/gatsby/src/components/Layout.js
@@ -1,18 +1,45 @@
 import React from 'react';
 import 'normalize.css';
+import styled from 'styled-components';
 import Footer from './Footer';
 import Nav from './Nav';
 import GlobalStyles from '../styles/GlobalStyles';
 import Typography from '../styles/Typography';
+import Stripes from '../assets/images/stripes.svg';
+
+const ContentStyles = styled.div`
+  background: white;
+  padding: 2rem;
+`;
+
+const SiteBordersStyles = styled.div`
+  max-width: 1000px;
+  margin: 12rem auto 4rem auto;
+  margin-top: clamp(2rem, 10vw, 12rem);
+  background: white url(${Stripes});
+  background-size: 1500px;
+  border: 5px solid white;
+  padding: 5px;
+  padding: clamp(5px, 1vw, 25px);
+  box-shadow: 0 0 5px 3px rgba(0, 0, 0, 0.044);
+  @media (max-width: 1100px) {
+    margin-left: 1.5rem;
+    margin-right: 1.5rem;
+  }
+`;
 
 export default function Layout({ children }) {
   return (
-    <div>
+    <>
       <GlobalStyles />
       <Typography />
-      <Nav />
-      {children}
+      <SiteBordersStyles>
+        <ContentStyles>
+          <Nav />
+          {children}
+        </ContentStyles>
+      </SiteBordersStyles>
       <Footer />
-    </div>
+    </>
   );
 }
