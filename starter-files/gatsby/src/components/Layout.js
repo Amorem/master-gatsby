17469afe10d0b0aeb00ca819a35c66596ddffef1
Amorem <dev@amorem.com>
2020-10-23 10:37:39 +0200

    Styling Nav

--- starter-files/gatsby/src/components/Layout.js
+++ starter-files/gatsby/src/components/Layout.js
@@ -3,11 +3,13 @@ import 'normalize.css';
 import Footer from './Footer';
 import Nav from './Nav';
 import GlobalStyles from '../styles/GlobalStyles';
+import Typography from '../styles/Typography';
 
 export default function Layout({ children }) {
   return (
     <div>
       <GlobalStyles />
+      <Typography />
       <Nav />
       {children}
       <Footer />
