d4bb8a98222f77df0559c49803fca2883ba95e9a
Amorem <dev@amorem.com>
2020-10-26 18:54:36 +0100

    CSS subgrid

--- starter-files/gatsby/src/pages/pizzas.js
+++ starter-files/gatsby/src/pages/pizzas.js
@@ -1,5 +1,39 @@
 import React from 'react';
+import { graphql } from 'gatsby';
+import PizzaList from '../components/PizzaList';
 
-export default function PizzasPage() {
-  return <p>Pizzas Page</p>;
+export default function PizzasPage({ data }) {
+  const pizzas = data.pizzas.edges;
+  return (
+    <>
+      <PizzaList pizzas={pizzas} />
+    </>
+  );
 }
+
+export const query = graphql`
+  query pizzaQuery {
+    pizzas: allSanityPizza {
+      edges {
+        node {
+          name
+          id
+          slug {
+            current
+          }
+          toppings {
+            name
+            id
+          }
+          image {
+            asset {
+              fluid(maxWidth: 400) {
+                ...GatsbySanityImageFluid
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+`;
